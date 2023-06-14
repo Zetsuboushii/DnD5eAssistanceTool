f42f322c1438fa9b4d81af8bfab37510d91e962c
Knick21 <nick.buettner04@gmail.com>
2023-06-14 23:19:12 +0200

    Added item.vue 🍞

---
+++ src/views/Item.vue
@@ -0,0 +1,30 @@
+<script>
+
+export default {
+  name: "drawer",
+  data() {
+    return {
+      testName: "Oni-Goroshi",
+      testDescription: "dwhdwudwhudaidshdsjdhudhwudhwudhwudhwdushadhwud",
+    }
+  }
+}
+
+
+</script>
+
+<template>
+
+<v-card>
+  {{testName}}
+</v-card>
+
+<v-card>
+  {{testDescription}}
+</v-card>
+
+</template>
+
+<style scoped>
+
+</style>
